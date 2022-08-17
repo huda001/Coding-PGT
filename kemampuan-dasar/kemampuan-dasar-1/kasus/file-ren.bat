53e5ad2c185111601af969143ed84562c470c509
AHMAD NUR HUDA <111000177+huda001@users.noreply.github.com>
2022-08-17 11:52:02 +0700

    Add files via upload

---
+++ kemampuan-dasar/kemampuan-dasar-1/kasus/file-ren.bat
@@ -0,0 +1,15 @@
+@echo off
+title cari-java.sh
+dir *.java
+echo.
+@echo pencarian berhasil!
+@echo Ada file java pada directori C:\Program Files (x86)\Java\jre1.8.0_341\legal\javafx
+echo.
+@echo icu_web.java diganti namanya (y/t)?
+@echo silahkan tekan y = ganti nama file
+@echo silahkan tekan t = tidak diganti
+echo.
+CHOICE /c YT /m "Ya atau Tidak"
+pause
+
+
